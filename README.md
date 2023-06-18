ffab12fe4d375b5bc7e6318bfe4060f868ccd4ae
Home Laptop <kaisonpham@gmail.com>
2023-06-17 23:27:24 -0700

    Added NFC landing page

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # personal-webpage
 connected to google cloud platform for hosting
 
-This project may be ported to Firebase...
-
 ## Notes:
 - The workflow for deploying the page to Google's App Engine has been deprecated, as Google's Build Trigger has replaced it's function.
--- www/js/default.js
+++ www/js/default.js
@@ -1,13 +1,40 @@
 // Shows the page content after all DOM elements load, and removes the loading animation
 document.onreadystatechange = function () {
-    var state = document.readyState
-    if (state == 'interactive') {
-        document.getElementById('page').style.visibility = "hidden";
-    } else if (state == 'complete') {
+    var state = document.readyState;
+    var pageDOM = document.getElementById("page");
+    var loadedDOM = document.getElementById("loaded");
+    var loadingDOM = document.getElementById("loading");
+    if (state == "interactive") {
+        pageDOM.style.transform = "translate(0, -100vh)";
+        pageDOM.style.visibility = "hidden";
+    } else if (state == "complete") {
+        loadedDOM.style.transitionDuration = "1s";
+        loadedDOM.style.height = "200vh";
+        loadedDOM.style.width = "200vh";
+        loadedDOM.style.transitionDuration = "none";
         setTimeout(function () {
-            document.getElementById('loading').style.visibility = "hidden";
-            document.getElementById('loading').remove();
-            document.getElementById('page').style.visibility = "visible";
-        }, 1000);
+            loadingDOM.style.visibility = "hidden";
+            loadingDOM.remove();
+            pageDOM.style.visibility = "visible";
+            pageDOM.style.transitionDuration = "1s";
+            pageDOM.style.transform = "translate(0, 0)";
+            pageDOM.style.transitionDuration = "none";
+        }, 600);
+        var targets = document.getElementsByClassName("copy-btn");
+        for (var i = 0; i < targets.length; i++) {
+            targets[i].addEventListener("click", (e) => {
+                copyText(e.target);
+            })
+        }
+    }
+};
+
+
+function copyText(target) {
+    try {
+        navigator.clipboard.writeText(target.textContent);
+    } catch {
+        target.focus();
+        target.select();
     }
 }
